2363df83516ce43321553220aed20a536ea58b5d
leocarv <leo_1497@hotmail.com>
2023-02-27 20:39:18 -0300

    configurações e navbar

---
+++ resources/js/Components/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Inicial</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Informações</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Contatos</a>
+        </li>
+        <li class="nav-item">
+            <a href="#">
+                <img src="../../../public/build/assets/perfil.jpg" alt="perfil" class="img-perfil">
+            </a>
+        </li>
+    </ul>
+
+</template>
+
+
+
+<script>
+
+export default {
+    name: "Navbar"
+}
+
+</script>
+
+<style>
+* {
+    margin: 0px;
+    border: 0;
+}
+
+.nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+
+    height: 50px;
+    width: 100%;
+
+    color: white;
+    background-color: rgba(178,0,0,255);
+}
+
+.img-perfil {
+    border-radius: 50%;
+    height: 40px;
+    width: 40px;
+}
+
+</style>
---
+++ resources/js/Pages/Inicial/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Navbar />
+    </div>
+
+</template>
+
+
+
+<script>
+import Navbar from '../../Components/Navbar.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        Navbar
+    }
+}
+
+</script>
