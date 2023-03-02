3a7b22dc525aad4a8906ae36a980d6aa191038e7
leocarv <leo_1497@hotmail.com>
2023-03-02 01:59:23 -0300

    criação de modelos

---
+++ database/migrations/2023_03_01_231855_create_comentarios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Postagem;
+use App\Models\Usuarios;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comentarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('descricao', 255);
+            $table->foreignIdFor(Usuarios::class);
+            $table->foreignIdFor(Postagem::class);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comentarios');
+    }
+};
