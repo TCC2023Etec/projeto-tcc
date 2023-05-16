3492f39e0ae377231b19451ce07e97651ebad4c3
leocarv <leo_1497@hotmail.com>
2023-05-15 23:53:13 -0300

    wpp btn

--- resources/js/Layouts/Admin.vue
+++ resources/js/Layouts/Admin.vue
@@ -1,13 +1,16 @@
 <template>
     <Navbar />
 
-    <div v-if="$page.props.flash.mensagem" class="toast align-items-center p-1 mb-4 text-sm bg-green-300 toast fade show" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="d-flex">
-            <div class="toast-body">
-                {{ $page.props.flash.mensagem }}
+    <div class="container">
+        <div v-if="$page.props.flash.mensagem" class="toast align-items-center p-1 mb-4 text-sm bg-green-300 toast fade show" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="d-flex">
+                <div class="toast-body">
+                    {{ $page.props.flash.mensagem }}
+                </div>
+                <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close">X</button>
             </div>
-            <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close">X</button>
         </div>
+        <div class="icon-wpp bx-lg"><a href="#"><i class='bx bxl-whatsapp'></i></a></div>
     </div>
 </template>
 
@@ -32,3 +35,14 @@ export default {
 }
 
 </script>
+
+<style>
+.icon-wpp {
+    position: fixed;
+    bottom: 10px;
+    right: 10px;
+
+    width: 50px;
+    height: 50px;
+}
+</style>
